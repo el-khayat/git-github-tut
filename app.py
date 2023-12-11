e87fc1c7732741cf1274fb674dfeaf3ecb8aa56a
el-khayat <elkhayat705@gmail.com>
2023-12-11 13:14:42 +0100

    [DEV-004] chnage float to int

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def add(a,b) :
 
 if __name__ == "__main__":
     print("Hello world !")
-    n =  float(   input("inter a number")   ) 
+    n =  int(   input("inter a number")   ) 
 
     n2 = calculate_square(n)
     print(f"result is {n2}")
