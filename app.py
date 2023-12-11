3b17fea00ac956fec67d6de8f9ba51537793267b
el-khayat <elkhayat705@gmail.com>
2023-12-11 13:13:17 +0100

    [DEV-002] define the mil function

--- app.py
+++ app.py
@@ -6,10 +6,15 @@ def calculate_square(number):
 def add(a,b) :
     return a+b ;
 
+def mul(a,b) :
+    return a*b ;
+
 
 if __name__ == "__main__":
     print("Hello world !")
-    n =  float(   input("inter a number")   ) 
+    ns = input("inter a number") 
+    
+    n =  float(  ns    ) 
 
     n2 = calculate_square(n)
     print(f"result is {n2}")
