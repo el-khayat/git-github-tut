dc9d666e340672dd00a163c43f492726b60dbf99
el-khayat <elkhayat705@gmail.com>
2023-12-11 12:39:08 +0100

    call the calculate square function

--- app.py
+++ app.py
@@ -6,4 +6,8 @@ def calculate_square(number):
 
 if __name__ == "__main__":
     print("Hello world !")
+    n = input("inter a number")
+
+    n2 = calculate_square(n)
+    print(f"result is {n2}")
     
